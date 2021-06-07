db3505b0f89875013958994da4dbe9d12255753e
Tim Johnson <johnsont53@gmail.com>
2021-06-07 11:57:41 -0400

    All book/author and publisher stuff

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -3,20 +3,47 @@ package guru.springframework.spring5webapp.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
 import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
 import guru.springframework.spring5webapp.repositories.PublisherRepository;
 
 @Component
 public class BootstrapData implements CommandLineRunner {
 
+	private final AuthorRepository authorRepository;
+	private final BookRepository bookRepository;
 	private final PublisherRepository publisherRepository;
 	
-	public BootstrapData (PublisherRepository publisherRepository) {
+	public BootstrapData (AuthorRepository authorRepository, 
+			BookRepository bookRepository, 
+			PublisherRepository publisherRepository) {
+		
+		this.authorRepository = authorRepository;
+		this.bookRepository = bookRepository;
 		this.publisherRepository = publisherRepository;
 	}
 	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Author author1 = new Author("Isaac", "Asimov");
+		Book book1 = new Book("Foundation", "1234");
+		author1.getBooks().add(book1);
+		book1.getAuthors().add(author1);
+		
+		authorRepository.save(author1);
+		bookRepository.save(book1);
+		
+		Author clark = new Author("Arthur", "Clark");
+		Book rama = new Book("Rama", "ab456");
+		clark.getBooks().add(rama);
+		rama.getAuthors().add(clark);
+		
+		authorRepository.save(clark);
+		bookRepository.save(rama);
 		
 		Publisher publisher = new Publisher ("MingLabs");
 
@@ -28,6 +55,8 @@ public class BootstrapData implements CommandLineRunner {
 		publisherRepository.save(publisher);
 		
 		System.out.println("Bootstrap data");
+		
+		System.out.println("Number of books: " + bookRepository.count());
 		
 		System.out.print("Numbe of publishers: " + publisherRepository.count());
 	}
